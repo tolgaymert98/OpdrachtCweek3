6a1dcd37b5fab5df68170be307ca19407dd5eedd
tolgaymert98 <18064523@student.hhs.nl>
2019-12-03 15:33:43 +0100

    Test

--- COpdrachtWeek3/Program.cs
+++ COpdrachtWeek3/Program.cs
@@ -3,10 +3,11 @@
 namespace COpdrachtWeek3
 {
     class Program
-    {jjjj
+    {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
